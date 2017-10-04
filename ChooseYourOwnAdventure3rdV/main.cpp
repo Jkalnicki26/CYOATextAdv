5bb7a218ff2cc0c5f0a6984e40d0705fa7472906
Sportsboss <jkalnicki26@yahoo.com>
2017-10-04 14:47:19 -0400

    Initial Commit

---
+++ ChooseYourOwnAdventure3rdV/main.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  ChooseYourOwnAdventure3rdV
+//
+//  Created by Jack Kalnicki on 9/28/17.
+//  Copyright © 2017 Jack Kalnicki. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int alive = 1;
+string input = "0";
+string room = "main";
+string name = "--";
+
+int main(int argc, const char * argv[]) {
+    while(input !="q" || room == "Win"){
+        cout << "Welcome to Choose Your Own Adventure!\n";
+        cout << "Please enter your name.\n";
+        cin >> name;
+        cout << "Okay " << name << " Get ready to play!\n";
+        if(room == "main"){
+            cout << "Hello? Hello? You hear from a voice in the distance. You are in a dark room.\n";
+            cout << "You have a few options. You can either look for the voice or turn on the lights";
+            cin >> input;
+            
+            
+            {
+}
+
+
+}
+
+
+
+
+
+
+
+    }}
+

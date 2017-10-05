808913cbbdf1418e57e2f22079e3031dffeb1fec
Sportsboss <jkalnicki26@yahoo.com>
2017-10-05 14:51:50 -0400

    first commit

--- ChooseYourOwnAdventure3rdV/main.cpp
+++ ChooseYourOwnAdventure3rdV/main.cpp
@@ -15,15 +15,25 @@ string room = "main";
 string name = "--";
 
 int main(int argc, const char * argv[]) {
+    cout << "Welcome to Choose Your Own Adventure!\n";
+    cout << "Please enter your name.\n";
+    cin >> name;
+    cout << "Okay " << name << " Get ready to play!\n";
     while(input !="q" || room == "Win"){
-        cout << "Welcome to Choose Your Own Adventure!\n";
-        cout << "Please enter your name.\n";
-        cin >> name;
-        cout << "Okay " << name << " Get ready to play!\n";
         if(room == "main"){
             cout << "Hello? Hello? You hear from a voice in the distance. You are in a dark room.\n";
             cout << "You have a few options. You can either look for the voice or turn on the lights";
             cin >> input;
+            if(input == "look"){
+                cout << "You have been looking for 30 minutes and you have found nothing. You are very tired. Do you want to sleep or try to stay awake.\n";
+                cin >> input;
+                if(input == "sleep"){
+                    cout << "You have been killed by something unknown.\n";
+                    input = "q";
+                    
+                }
+            }
+                
             
             
             {
